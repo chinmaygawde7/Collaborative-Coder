d484c1bc569a628aa9863c26c9f19cd8ddba6b3d
Chinmay007 <gawdechinmay007@gmail.com>
2020-09-21 15:37:11 +0530

    Basic is done

---
+++ textcircle.js
@@ -0,0 +1,41 @@
+this.Documents = new Mongo.Collection("documents")
+
+
+
+if (Meteor.isClient) {
+
+	Template.editor.helpers({
+		docid: function () {
+			console.log("doc id helper:");
+			console.log(Documents.findOne());
+			var doc = Documents.findOne();
+			if (doc) {
+				return doc._id;
+			} else {
+				return undefined;
+			}
+
+		},
+
+		config: function () {
+			return function (editor) {
+				editor.on("change", function (cm_editor, info) {
+
+				$("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+
+				});
+            }
+        }
+	});
+
+}
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		// code to run on server at startup
+
+		if (!Documents.findOne()) { // no documents yet
+			Documents.insert({ title: "my new document" });
+        }
+	})
+}
